cacd0e4c5c46b0cb86db1fe17f1d76a87aeb752b
Rahaf Malas <malasrahaf1@gmail.com>
2022-08-23 15:38:38 +0300

    fix Category screen

--- lib/ui/categories/categories_provider.dart
+++ lib/ui/categories/categories_provider.dart
@@ -1,23 +1,39 @@
 import 'dart:convert';
 
 import 'package:cell_avenue_store/models/category.dart';
-import 'package:cell_avenue_store/models/product.dart';
 import 'package:cell_avenue_store/utilities/constants.dart';
 import 'package:cell_avenue_store/utilities/http_requests.dart';
 import 'package:flutter/material.dart';
 
-class CategoryProvider with ChangeNotifier {
+class CategoriesProvider with ChangeNotifier {
+  int value = 0;
+  int index = 0;
   List<Category> categories = [];
-  Future<List<Category>> loadCategories(context) async {
+  changeValue(int index) {
+    value = index;
+    //  notifyListeners();
+  }
+  // int get value => _value;
+  // int get index => _index;
+  // List<Category> get categories => _categories;
+
+  Future<List<Category>>? loadCategories(context) async {
+    if (categories.isNotEmpty) return categories;
+
+    print("object++++++++----");
     var url = Constants.BASE_URL +
         Constants.categories +
         Constants.wooAuth +
         '&per_page=50&hide_empty=true' +
         '&status=publish&stock_status=instock';
     await HttpRequests.httpGetRequest(context, url, {}, (value, map) {
+      print("object------------------------------------++++");
+
       List list = json.decode(value);
       categories.addAll(List.from(list.map((e) => Category.fromMap(e))));
+      print("======" + "$categories");
     }, () {});
+    print("object_--_-------");
     return categories;
   }
 }
